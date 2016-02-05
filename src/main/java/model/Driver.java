cb76b06b09b950b10aa4288f2f5ee59777d554ca
luizcapu <luizcapu@gmail.com>
2016-02-05 16:54:22 -0200

    test case

--- src/main/java/model/Driver.java
+++ src/main/java/model/Driver.java
@@ -1,8 +1,17 @@
 package model;
 
+import java.util.Map;
+
 public class Driver {
 	public Integer id;
 	public Double latitude;
 	public Double longitude;
 	public boolean available;
+	
+	public void populate(Map<String, Object> data) {
+		this.id = ((Number) data.get("id")).intValue();
+		this.latitude = (Double) data.get("latitude");
+		this.longitude = (Double) data.get("longitude");
+		this.available = (Boolean) data.get("available");
+	}
 }
--- src/test/java/com/taxi_api/DriverResourceTest.java
+++ src/test/java/com/taxi_api/DriverResourceTest.java
@@ -1,6 +1,8 @@
 package com.taxi_api;
 
+
 import java.util.List;
+import java.util.Map;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -16,6 +18,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.google.gson.Gson;
+
 import static org.junit.Assert.assertEquals;
 
 public class DriverResourceTest {
@@ -27,6 +31,7 @@ public class DriverResourceTest {
 			+ "{\"id\":2,\"latitude\":-30.123,\"longitude\":-32.432,\"available\":false},"
 			+ "{\"id\":3,\"latitude\":-40.123,\"longitude\":-42.432,\"available\":true},"
 			+ "{\"id\":4,\"latitude\":-50.123,\"longitude\":-52.432,\"available\":true}]";
+	private final Gson gson = new Gson();
 
     @Before
     public void setUp() throws Exception {
@@ -35,13 +40,6 @@ public class DriverResourceTest {
         server.start();
         // create the client
         Client c = ClientBuilder.newClient();
-
-        // uncomment the following line if you want to enable
-        // support for JSON in the client (you also have to uncomment
-        // dependency on jersey-media-json module in pom.xml and Main.startServer())
-        // --
-        // c.configuration().enable(new org.glassfish.jersey.media.json.JsonJaxbFeature());
-
         target = c.target(Main.BASE_URI);
     }
 
@@ -50,12 +48,18 @@ public class DriverResourceTest {
         server.shutdownNow();
     }
 
+	private static void compareDrivers(Driver d1, Driver d2) {
+		assertEquals(d1.id, d2.id);
+		assertEquals(d1.latitude, d2.latitude);
+		assertEquals(d1.longitude, d2.longitude);
+		assertEquals(d1.available, d2.available);
+	}
+    
     @SuppressWarnings("unchecked")
 	@Test
     public void testEndPointsFlow() {
     	Response r;
     	List<Driver> drivers;
-    	Driver driver;
     	
     	r = target.path("driver/1/status").request().get();
     	assertEquals(r.getStatus(), 204);
@@ -65,8 +69,47 @@ public class DriverResourceTest {
     	assertEquals(r.getStatus(), 200);
     	assertEquals(drivers.size(), 0);
     	
-    	r = target.path("driver/1/status").request().post(Entity.entity(new Driver(), MediaType.APPLICATION_JSON));
+    	List<Map<String, Object>> payloadList = gson.fromJson(PAYLOAD, List.class);
+    	for (Map<String, Object> item : payloadList) {
+    		Driver driver = new Driver();
+        	driver.populate(item);
+
+    		System.out.println("Testing driver " + driver.id);        	
+    		
+    		//-- BEGIN test insert 
+    		r = target.path("driver/"+driver.id+"/status").request().put(Entity.entity(driver, MediaType.APPLICATION_JSON));
+        	assertEquals(r.getStatus(), 200);
+        	assertEquals(r.readEntity(String.class), EMPTY_STR);
+    		//-- END test insert
+        	
+    		// BEGIN compare after insert
+        	r = target.path("driver/"+driver.id+"/status").request().get();
+        	assertEquals(r.getStatus(), 200);
+        	DriverResourceTest.compareDrivers(driver, r.readEntity(Driver.class));
+    		// END compare after insert
+
+    		//-- BEGIN test update 
+        	driver.latitude += 10;
+        	driver.longitude += 5;
+        	driver.available = !driver.available;
+    		r = target.path("driver/"+driver.id+"/status").request().post(Entity.entity(driver, MediaType.APPLICATION_JSON));
+        	assertEquals(r.getStatus(), 200);
+        	assertEquals(r.readEntity(String.class), EMPTY_STR);
+    		//-- END test update
+        	
+    		// BEGIN compare after update
+        	r = target.path("driver/"+driver.id+"/status").request().get();
+        	assertEquals(r.getStatus(), 200);
+        	//DriverResourceTest.compareDrivers(driver, r.readEntity(Driver.class));
+    		// END compare after update        	
+    	}    	
+
+		// BEGIN fetch drivers
+		System.out.println("Fetching all drivers");        	
+    	r = target.path("drivers").request().get();
+    	drivers = r.readEntity(List.class);
     	assertEquals(r.getStatus(), 200);
-    	assertEquals(r.readEntity(String.class), EMPTY_STR);
+    	assertEquals(drivers.size(), payloadList.size());
+		// END fetch drivers    	
     }
 }
